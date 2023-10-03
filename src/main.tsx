fee23780948324bff445e9b072f91f0d2b47a38f
VanTanh <lytanh@starboard-asia.com>
2023-10-03 17:31:03 +0700

    create navbar ui

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Project from './components/Project';
-import Contact from './components/Contact';
-import Skills from './components/Skills';
+import Layout from './components/Layout';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Router>
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/projects' element={<Project />} />
-            <Route path="/skills" element={<Skills />} />
-            <Route path="/contact" element={<Contact />} />
+            <Route path="/" element={<Layout />} />
         </Routes>
     </Router>
 )
